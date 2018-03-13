58bdb89a12026d88712b313ccc7bc8fbc0cae455
Joeri <jvh@jvh.be>
2018-03-13 09:56:44 +0100

    updated deploy scripts

--- deploy-blue.sh
+++ deploy-blue.sh
@@ -17,6 +17,7 @@ set -e
 STATE="terraform.tfstate"
 PLAN="terraform.plan"
 ANSIBLEWEBINVENTORY="ansible-blue/inventory/web"
+ANSIBLESQLINVENTORY="ansible-blue/inventory/sql"
 
 TODAY=`date +"%Y-%m-%d"`
 
@@ -38,11 +39,21 @@ echo ""
 terraform apply "$PLAN"
 
 echo ""
-echo "==> Terraform output to Ansible inventory"
+echo "==> Terraform output to Ansible web inventory"
 echo ""
-terraform output -state="$STATE" ansible_web_inventory > "$ANSIBLEWEBINVENTORY"
+terraform output -state="$STATE" web_ansible_inventory > "$ANSIBLEWEBINVENTORY"
 
 echo ""
-echo "==> Ansible configuration"
+echo "==> Terraform output to Ansible sql inventory"
+echo ""
+terraform output -state="$STATE" sql_ansible_inventory > "$ANSIBLESQLINVENTORY"
+
+echo ""
+echo "==> Ansible configuration web server"
 echo ""
 ansible-playbook ansible-blue/deploy-docker.yml -vvv -i "$ANSIBLEWEBINVENTORY"
+
+echo ""
+echo "==> Ansible configuration sql server"
+echo ""
+ansible-playbook ansible-blue/deploy-docker.yml -vvv -i "$ANSIBLESQLINVENTORY"
