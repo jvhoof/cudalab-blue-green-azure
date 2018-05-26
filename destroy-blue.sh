6d0790a161a3f263127b8b328608183e18a19f2e
Joeri <jvh@jvh.be>
2018-05-26 16:54:24 +0200

    DEBUG destroy

--- destroy-blue.sh
+++ destroy-blue.sh
@@ -36,4 +36,4 @@ terraform init \
 echo ""
 echo "==> Terraform destory"
 echo ""
-terraform destroy terraform-blue/
+terraform destroy -var "CCSECRET=$CCSECRET" -var "PASSWORD=$PASSWORD" -var "SSH_KEY_DATA=$SSH_KEY_DATA" terraform-blue/
