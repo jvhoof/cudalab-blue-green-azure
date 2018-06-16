2ac44d980e69f1ba5c03018b8618880a0f3821d3
Joeri <jvh@jvh.be>
2018-06-16 20:32:31 +0200

    Removed php connection to DB for now

--- ansible/roles/colors/files/colors/html/blue/index.php
+++ ansible/roles/colors/files/colors/html/blue/index.php
@@ -22,54 +22,54 @@ $rowstart = 2;
 for ( $rowcnt = 0 ; $rowcnt < $rows ; $rowcnt++ ) {
 	echo '<tr>';
 	for ( $colcnt = 0 ; $colcnt < $cols ; $colcnt++ ) {
-		if ( $rowcnt==$rowstart && $colcnt==$colstart ) {
-			echo "<td colspan=$colspan rowspan=$rowspan >";
-			$mysqli = new mysqli('172.16.0.5', 'demo', '', 'demo');
+//		if ( $rowcnt==$rowstart && $colcnt==$colstart ) {
+//			echo "<td colspan=$colspan rowspan=$rowspan >";
+//			$mysqli = new mysqli('172.30.105.196', 'demo', '', 'demo');
 			
 			// Oh no! A connect_errno exists so the connection attempt failed!
-			if ($mysqli->connect_errno) {
+//			if ($mysqli->connect_errno) {
 				// The connection failed. What do you want to do? 
 				// You could contact yourself (email?), log the error, show a nice page, etc.
 				// You do not want to reveal sensitive information
 			
 				// Let's try this:
-				echo "Unable to connect to the database";
+//				echo "Unable to connect to the database";
 			
 				// Something you should not do on a public site, but this example will show you
 				// anyways, is print out MySQL error related information -- you might log this
-				echo "Error: Failed to make a MySQL connection, here is why: \n";
-				echo "Errno: " . $mysqli->connect_errno . "\n";
-				echo "Error: " . $mysqli->connect_error . "\n";
-			}
-			?>
+//				echo "Error: Failed to make a MySQL connection, here is why: \n";
+//				echo "Errno: " . $mysqli->connect_errno . "\n";
+//				echo "Error: " . $mysqli->connect_error . "\n";
+//			}
+//			?>
 <form>
 <input name="needle"><input type="submit" value="Search color"><br>
 </form>
 			<?php
-			if (!empty( $_GET[ 'needle' ])) {
-				$needle = $_GET[ 'needle' ];	
-				$sql = "SELECT * FROM colors WHERE name LIKE '$needle'";
-				if (!$result = $mysqli->query($sql)) {
-					// Oh no! The query failed. 
-					echo "Sorry, the website is experiencing problems.";
-				} else {
-					if (!$result = $mysqli->query($sql)) {
-						echo "Sorry, the website is experiencing problems.";
-					} else {
-						if ($result && $result->num_rows) {  
-							// just one match - display it
-							$hexid = $result->fetch_assoc()['hexid'];  
-							$color = $result->fetch_assoc()['name'];  
-							echo '<div style="background-color: #' . $hexid;
-							echo '; width: 80px; height: 80px; float: right; border: 1px solid black;">&nbsp;</div>';
-							echo '<div style="font-size:4em;float: left;">' . $color;
-							echo '</div>';
-						}
-					}
-				}
-			}
-			echo "</td>";
-		}
+//			if (!empty( $_GET[ 'needle' ])) {
+//				$needle = $_GET[ 'needle' ];	
+//				$sql = "SELECT * FROM colors WHERE name LIKE '$needle'";
+//				if (!$result = $mysqli->query($sql)) {
+//					// Oh no! The query failed. 
+//					echo "Sorry, the website is experiencing problems.";
+//				} else {
+//					if (!$result = $mysqli->query($sql)) {
+//						echo "Sorry, the website is experiencing problems.";
+//					} else {
+//						if ($result && $result->num_rows) {  
+//							// just one match - display it
+//							$hexid = $result->fetch_assoc()['hexid'];  
+//							$color = $result->fetch_assoc()['name'];  
+//							echo '<div style="background-color: #' . $hexid;
+//							echo '; width: 80px; height: 80px; float: right; border: 1px solid black;">&nbsp;</div>';
+//							echo '<div style="font-size:4em;float: left;">' . $color;
+//							echo '</div>';
+//						}
+//					}
+//				}
+//			}
+//			echo "</td>";
+//		}
 		if ( $colcnt >= $colstart && $colcnt < ( $colstart + $colspan ) && $rowcnt >= $rowstart && $rowcnt < ( $rowstart+$rowspan )) {
 			$colcnt += $colspan;
 		}
